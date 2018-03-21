96912c3437c1ea6c9675155bfa9be83ba2694a8e
tribetmen <dima.koloskov.1997@yandex.ru>
2018-03-21 18:18:15 +0300

    Made beautifully

--- hx8379_dsi_vdo_6572.c
+++ hx8379_dsi_vdo_6572.c
@@ -38,18 +38,18 @@
 static LCM_UTIL_FUNCS lcm_util = {0};
 
 #define SET_RESET_PIN(v)    								(lcm_util.set_reset_pin((v)))
-#define UDELAY(n) 											(lcm_util.udelay(n))
-#define MDELAY(n) 											(lcm_util.mdelay(n))
+#define UDELAY(n) 									(lcm_util.udelay(n))
+#define MDELAY(n) 									(lcm_util.mdelay(n))
 
 // ---------------------------------------------------------------------------
 //  Local Functions
 // ---------------------------------------------------------------------------
 #define dsi_set_cmdq_V2(cmd, count, ppara, force_update)	lcm_util.dsi_set_cmdq_V2(cmd, count, ppara, force_update)
 #define dsi_set_cmdq(pdata, queue_size, force_update)		lcm_util.dsi_set_cmdq(pdata, queue_size, force_update)
-#define wrtie_cmd(cmd)										lcm_util.dsi_write_cmd(cmd)
-#define write_regs(addr, pdata, byte_nums)					lcm_util.dsi_write_regs(addr, pdata, byte_nums)
-#define read_reg(cmd)										lcm_util.dsi_dcs_read_lcm_reg(cmd)
-#define read_reg_v2(cmd, buffer, buffer_size)   			lcm_util.dsi_dcs_read_lcm_reg_v2(cmd, buffer, buffer_size)
+#define wrtie_cmd(cmd)						lcm_util.dsi_write_cmd(cmd)
+#define write_regs(addr, pdata, byte_nums)			lcm_util.dsi_write_regs(addr, pdata, byte_nums)
+#define read_reg(cmd)						lcm_util.dsi_dcs_read_lcm_reg(cmd)
+#define read_reg_v2(cmd, buffer, buffer_size)   		lcm_util.dsi_dcs_read_lcm_reg_v2(cmd, buffer, buffer_size)
 
 struct LCM_setting_table {
 	unsigned char cmd;
@@ -109,102 +109,102 @@ static void lcm_get_params(LCM_PARAMS *params)
 
 static void lcm_init_registers(void)
 {
-  unsigned int data_array[16];
+        unsigned int data_array[16];
 
 	// SET password
-  data_array[0] = 0x00043902;
-  data_array[1] = 0x7983FFB9;
-  dsi_set_cmdq(data_array, 2, 1);
+        data_array[0] = 0x00043902;
+        data_array[1] = 0x7983FFB9;
+        dsi_set_cmdq(data_array, 2, 1);
 
 	// SET Display 480x800
-  data_array[0] = 0x00033902;
+        data_array[0] = 0x00033902;
 	data_array[1] = 0x009351BA;
-  dsi_set_cmdq(data_array, 2, 1);
+        dsi_set_cmdq(data_array, 2, 1);
 
 	// SET Display column inversion
-  data_array[0] = 0x00143902;
-  data_array[1] = 0x245000B1;
-  data_array[2] = 0x11088DEA;
-  data_array[3] = 0x2F271711;
-  data_array[4] = 0x0B421A9A;
-  data_array[5] = 0xE600F14A;
-  dsi_set_cmdq(data_array, 6, 1);
+        data_array[0] = 0x00143902;
+        data_array[1] = 0x245000B1;
+        data_array[2] = 0x11088DEA;
+        data_array[3] = 0x2F271711;
+        data_array[4] = 0x0B421A9A;
+        data_array[5] = 0xE600F14A;
+        dsi_set_cmdq(data_array, 6, 1);
 
 	// SET GIP
-  data_array[0] = 0x000E3902;
-  data_array[1] = 0x3C0000B2;
-  data_array[2] = 0x22190408;
+        data_array[0] = 0x000E3902;
+        data_array[1] = 0x3C0000B2;
+        data_array[2] = 0x22190408;
 	data_array[3] = 0x0408FF00;
-  data_array[4] = 0x00002019;
-  dsi_set_cmdq(data_array, 5, 1);
+        data_array[4] = 0x00002019;
+        dsi_set_cmdq(data_array, 5, 1);
 
 	// SET Power
-  data_array[0] = 0x00203902;
-  data_array[1] = 0x000880B4;
-  data_array[2] = 0x32031032;
-  data_array[3] = 0x10327013;
-  data_array[4] = 0x28013708;
-  data_array[5] = 0x30003707;
-  data_array[6] = 0x083C3814;
-  data_array[7] = 0x28084000;
-  data_array[8] = 0x04303008;
-  dsi_set_cmdq(data_array, 9, 1);
+	data_array[0] = 0x00203902;
+        data_array[1] = 0x000880B4;
+        data_array[2] = 0x32031032;
+        data_array[3] = 0x10327013;
+        data_array[4] = 0x28013708;
+        data_array[5] = 0x30003707;
+        data_array[6] = 0x083C3814;
+        data_array[7] = 0x28084000;
+        data_array[8] = 0x04303008;
+        dsi_set_cmdq(data_array, 9, 1);
 
 	// SET Panel
-  data_array[0] = 0x00303902;
-  data_array[1] = 0x0A0000D5;
-  data_array[2] = 0x00050100;
-  data_array[3] = 0x88880003;
-  data_array[4] = 0x01238888;
-  data_array[5] = 0x13024567;
+        data_array[0] = 0x00303902;
+        data_array[1] = 0x0A0000D5;
+        data_array[2] = 0x00050100;
+        data_array[3] = 0x88880003;
+        data_array[4] = 0x01238888;
+        data_array[5] = 0x13024567;
 	data_array[6] = 0x88888888;
 	data_array[7] = 0x88888888;
-  data_array[8] = 0x76548888;
-  data_array[9] = 0x20313210;
+        data_array[8] = 0x76548888;
+        data_array[9] = 0x20313210;
 	data_array[10] = 0x88888888;
 	data_array[11] = 0x00008888;
-  data_array[12] = 0x00000000;
-  dsi_set_cmdq(data_array, 13, 1);
+        data_array[12] = 0x00000000;
+        dsi_set_cmdq(data_array, 13, 1);
 
 	// SET GAMMA
 	data_array[0] = 0x00243902;
-  data_array[1] = 0x000079E0;
-  data_array[2] = 0x3F2F2C02;
-  data_array[3] = 0x0E074927;
-  data_array[4] = 0x1416140F;
+        data_array[1] = 0x000079E0;
+        data_array[2] = 0x3F2F2C02;
+        data_array[3] = 0x0E074927;
+        data_array[4] = 0x1416140F;
 	data_array[5] = 0x00181316;
 	data_array[6] = 0x2F2C0200;
-  data_array[7] = 0x0749273F;
-  data_array[8] = 0x16140F0E;
+        data_array[7] = 0x0749273F;
+        data_array[8] = 0x16140F0E;
 	data_array[9] = 0x18131614;
-  dsi_set_cmdq(data_array, 10, 1);
+        dsi_set_cmdq(data_array, 10, 1);
 
 	// SET pixel format RGB888
-  data_array[0] = 0x00033902;
+        data_array[0] = 0x00033902;
 	data_array[1] = 0x000200C6;
-  dsi_set_cmdq(data_array, 2, 1);
+        dsi_set_cmdq(data_array, 2, 1);
 
 	// SET MIPI (2 Lane)
-  data_array[0] = 0x00033902;
+        data_array[0] = 0x00033902;
 	data_array[1] = 0x000002CC;
-  dsi_set_cmdq(data_array, 2, 1);
+        dsi_set_cmdq(data_array, 2, 1);
 
 	// Enable TE
-  data_array[1] = 0x00A000B6;
-  data_array[2] = 0x000000A0;
-  dsi_set_cmdq(data_array, 3, 1);
+        data_array[1] = 0x00A000B6;
+        data_array[2] = 0x000000A0;
+        dsi_set_cmdq(data_array, 3, 1);
 
 	// Sleep Out
-  data_array[0] = 0x00110500;
-  dsi_set_cmdq(data_array, 1, 1);
+        data_array[0] = 0x00110500;
+        dsi_set_cmdq(data_array, 1, 1);
 
-  MDELAY(200);
+        MDELAY(200);
 	
 	// Display On
-  data_array[0] = 0x00290500;
-  dsi_set_cmdq(data_array, 1, 1);
+        data_array[0] = 0x00290500;
+        dsi_set_cmdq(data_array, 1, 1);
 
-  MDELAY(30);
+        MDELAY(30);
 	
 }
 
@@ -253,11 +253,11 @@ static void lcm_setbacklight(unsigned int level)
 	unsigned int data_array[16];
 
 	if (level > 255)
-      level = 255;
+            level = 255;
 	printk("%s, %d\n", "lcm_setbacklight", level);
-  data_array[0]= 0x00023902;
-  data_array[1] =(0x51|(level<<8));
-  dsi_set_cmdq(data_array, 2, 1);
+        data_array[0]= 0x00023902;
+        data_array[1] =(0x51|(level<<8));
+        dsi_set_cmdq(data_array, 2, 1);
 }
 
 // ---------------------------------------------------------------------------
@@ -267,34 +267,34 @@ static void lcm_setbacklight(unsigned int level)
 static unsigned int lcm_compare_id(void)
 {
 
-  unsigned int id = 0;
-  unsigned char buffer[2];
-  unsigned int array[16];
+        unsigned int id = 0;
+        unsigned char buffer[2];
+        unsigned int array[16];
 
-  SET_RESET_PIN(1);
-  MDELAY(1);
-  SET_RESET_PIN(0);
-  MDELAY(10);
-  SET_RESET_PIN(1);
-  MDELAY(120);
-  array[0] = 0x00043902;
-  array[1] = 0x7983FFB9;
-  dsi_set_cmdq(&array, 2, 1);
-  MDELAY(10);
-  array[1] = 0x009351BA;
-  array[0] = 0x00033902;
-  dsi_set_cmdq(&array, 2, 1);
-  array[0] = 0x00013700;
-  dsi_set_cmdq(&array, 1, 1);
-  MDELAY(10);
+        SET_RESET_PIN(1);
+        MDELAY(1);
+        SET_RESET_PIN(0);
+        MDELAY(10);
+        SET_RESET_PIN(1);
+        MDELAY(120);
+        array[0] = 0x00043902;
+        array[1] = 0x7983FFB9;
+        dsi_set_cmdq(&array, 2, 1);
+        MDELAY(10);
+        array[1] = 0x009351BA;
+        array[0] = 0x00033902;
+        dsi_set_cmdq(&array, 2, 1);
+        array[0] = 0x00013700;
+        dsi_set_cmdq(&array, 1, 1);
+        MDELAY(10);
 
-  read_reg_v2(0xF4, buffer, 1);
-  id = buffer[0];
-  printk("lcd id %x %x\n", __func__, id);
+        read_reg_v2(0xF4, buffer, 1);
+        id = buffer[0];
+        printk("lcd id %x %x\n", __func__, id);
 #if 0
-  return (0x79 == id)?1:0;
+        return (0x79 == id)?1:0;
 #else
-  return 1;
+        return 1;
 #endif
 } 
 
@@ -303,7 +303,7 @@ static unsigned int lcm_compare_id(void)
 // ---------------------------------------------------------------------------
 LCM_DRIVER hx8379_dsi_vdo_6572_lcm_drv = 
 {
-	.name		= "hx8379_dsi_vdo_6572",
+        .name		= "hx8379_dsi_vdo_6572",
 	.set_util_funcs = lcm_set_util_funcs,
 	.get_params     = lcm_get_params,
 	.init           = lcm_init,
